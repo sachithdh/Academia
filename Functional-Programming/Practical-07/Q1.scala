6c0f9b0b15544b429bf61f8a5d4bba889183e4b3
sacheex <sachithdhanushka00@gmail.com>
2024-08-03 21:09:42 +0530

    Submission for practical 07

---
+++ Functional-Programming/Practical-07/Q1.scala
@@ -0,0 +1,10 @@
+def filterEvenNumbers(intList: List[Int]): List[Int] ={
+    intList.filter((i) => i % 2 == 0)
+}
+
+@main def Q1(): Unit ={
+    val intList: List[Int] = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val evenList = filterEvenNumbers(intList)
+    evenList.foreach(println)
+    
+}
