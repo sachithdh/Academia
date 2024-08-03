38ab3ddfff2e0776daf2e072e5410cb2b466edd7
sacheex <sachithdhanushka00@gmail.com>
2024-08-03 21:40:24 +0530

    Submission for practical 07

--- Functional-Programming/Practical-07/Q2.scala
+++ Functional-Programming/Practical-07/Q2.scala
@@ -5,6 +5,8 @@ def calculateSquare(intList: List[Int]): List[Int] ={
 @main def Q1(): Unit ={
     val intList: List[Int] = List(1, 2, 3, 4, 5)
     val squareList = calculateSquare(intList)
-    squareList.foreach(println)
+    println(s"Input List: $intList")
+    println(s"Output List: $squareList")
+    // squareList.foreach(println)
     
 }
