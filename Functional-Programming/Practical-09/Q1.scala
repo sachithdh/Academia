05a69a50d25868b0e430e6e293dc967e5390b2d0
sacheex <sachithdhanushka00@gmail.com>
2024-08-19 23:12:01 +0530

    Change output style

--- Functional-Programming/Practical-09/Q1.scala
+++ Functional-Programming/Practical-09/Q1.scala
@@ -1,5 +1,5 @@
 @main def Q1(): Unit ={
-    val interest: (Int) => Double = (deposit: Int) => {
+    val interest: (Double) => Double = (deposit: Double) => {
         deposit match 
             case x if (x <= 20000) => x * 0.02
             case x if (x <= 200000) => x * 0.035
@@ -7,5 +7,7 @@
             case x => x * 0.065
     }
 
-    println(interest(100000))
+    val deposit: Double = 500000
+    println(s"\nDeposit: Rs.$deposit, Interest: Rs.${interest(deposit)}")
 }
+
